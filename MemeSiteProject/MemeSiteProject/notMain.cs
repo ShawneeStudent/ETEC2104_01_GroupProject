9c2fed363fb989c20bf2075fb01e236ac6804691
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-02-13 12:12:39 -0500

    Changed the printouts in the main file
    Changed the Console.WriteLine to be more descriptive of what it is actually printing, also added the implementations of all the different tables, now need a way to print everything out to check it all works

--- MemeSiteProject/MemeSiteProject/notMain.cs
+++ MemeSiteProject/MemeSiteProject/notMain.cs
@@ -16,16 +16,21 @@ namespace MemeSiteProject
             conn.Open();
             string[] s = {
             "create table users (uid integer primary key, email text, picture text, name text, lastlogin integer);",
-            "create table posts (uid integer, pid integer primary key, forumid integer, parentid integer, content text)",
-            "create table comment (uid integer,commentID integer primary key,postID integer,date integer,data text)",
+            "create table posts (uid integer, postID integer primary key, forumid integer, parentid integer, content text)",
+            "create table comment (uid integer, commentID integer primary key, postID integer, date integer, data text)",
             "create table tag (uid integer, tagID integer primary key)",
-            "create table rating (uid integer, pid integer, rating integer)",
-            "create table view (uid integer, pid integer, date integer, ipAddress text)",
+            "create table rating (uid integer, postID integer, rating integer)",
+            "create table view (uid integer, postID integer, date integer, ipAddress text)",
 
             "insert into users (uid,email,picture,name,lastlogin) values (0,'postmaster@example.com','keyboardcat.jpg','admin',20190213)",
-            "insert into posts (uid,pid,forumid,parentid,content) values (0,0,1,NULL,'First post')",
-            "insert into comment(uid,commentID,postID,date,data) values (0,1,0,20190213,'Hi Jim')"
-            //"insert into ...
+            "insert into users (uid,email,picture,name,lastlogin) values (1337,'myEmail@example.com','profilepic.png','Walter',20190213)",
+            "insert into posts (uid, postID,forumid,parentID,content) values (1337,1,1,NULL,'Hello Peeps')",
+            "insert into posts (uid, postID,forumid,parentID,content) values (1337,2,1,NULL,'Shaggy at 0.1% of his power level')",
+            "insert into posts (uid,postID,forumid,parentid,content) values (0,0,1,NULL,'First post')",
+            "insert into comment (uid,commentID,postID,date,data) values (0,1,0,20190213,'Hi Jim')",
+            "insert into tag (uid,tagID) values (0, 13)",
+            "insert into rating (uid,postID,rating) values (0,0,5)",
+            "insert into view (uid,postID,date,ipAddress) values (0,0,20190214,'192.1337.7357')"
         };
 
             SQLiteCommand cmd;
@@ -36,8 +41,8 @@ namespace MemeSiteProject
                 cmd.ExecuteNonQuery();
             }
 
-
-            cmd = new SQLiteCommand("select content from posts where uid = 100;", conn);
+          
+            cmd = new SQLiteCommand("select content from posts where uid = 0;", conn);
             SQLiteDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
@@ -46,22 +51,22 @@ namespace MemeSiteProject
 
             Console.WriteLine("--------------------");
             cmd = new SQLiteCommand("select name,uid from users where name = $name;", conn);
-            cmd.Parameters.AddWithValue("$name", "alice");
+            cmd.Parameters.AddWithValue("$name", "Walter");
             rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                Console.WriteLine("SELECT RESULT 2: " + rdr["name"] + " " + rdr["uid"]);
+                Console.WriteLine("SELECT RESULT 2: " + rdr["name"] + " id:" + rdr["uid"]);
             }
             Console.WriteLine("--------------------");
 
             //assume n is some string
-            cmd = new SQLiteCommand("select users.name as BLAH, count(posts.pid) as BOOM from users " +
+            cmd = new SQLiteCommand("select users.name as BLAH, count(posts.postID) as BOOM from users " +
             "left outer join posts on users.uid=posts.uid group by users.name " +
-            "order by count(posts.pid) desc;", conn);
+            "order by count(posts.postID) desc;", conn);
             rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                Console.WriteLine("OUTER JOIN RESULT: " + rdr["BLAH"] + " " + rdr["BOOM"]);
+                Console.WriteLine("OUTER JOIN RESULT(User): " + rdr["BLAH"] + " | amount of posts: " + rdr["BOOM"]);
             }
 
             conn.Close();
