1b59c9db14f77d48e06d9470cbc25b7d412008ce
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-02-13 11:47:25 -0500

    tables added
    Added tables to the project, still needs to be able to print out and also created

--- MemeSiteProject/MemeSiteProject/notMain.cs
+++ MemeSiteProject/MemeSiteProject/notMain.cs
@@ -16,15 +16,16 @@ namespace MemeSiteProject
             conn.Open();
             string[] s = {
             "create table users (uid integer primary key, email text, picture text, name text, lastlogin integer);",
-            "create table posts (uid integer, id integer primary key, forumid integer, parentid integer, content text)",
-            "create table comment (uid integer primary key, postID integer, userID integer)",
-            "create table tag (uid integer primaryKey, content text)",
-            "create table rating (userID integer, postID integer, rating integer)",
-            "create table view (userID integer, postID integer, date integer, ipAddress text)",
+            "create table posts (uid integer, pid integer primary key, forumid integer, parentid integer, content text)",
+            "create table comment (uid integer,commentID integer primary key,postID integer,date integer,data text)",
+            "create table tag (uid integer, tagID integer primary key)",
+            "create table rating (uid integer, pid integer, rating integer)",
+            "create table view (uid integer, pid integer, date integer, ipAddress text)",
 
-            "insert into users (uid,name,email,lastlogin) values (0,'admin','postmaster@example.com',20140304)",
-            "insert into forums (creator,title,id) values (0,'sales',4)",
-            "insert into posts (uid,id,forumid,parentid,content) values (0,0,1,NULL,'First post')",
+            "insert into users (uid,email,picture,name,lastlogin) values (0,'postmaster@example.com','keyboardcat.jpg','admin',20190213)",
+            "insert into posts (uid,pid,forumid,parentid,content) values (0,0,1,NULL,'First post')",
+            "insert into comment(uid,commentID,postID,date,data) values (0,1,0,20190213,'Hi Jim')"
+            //"insert into ...
         };
 
             SQLiteCommand cmd;
@@ -54,9 +55,9 @@ namespace MemeSiteProject
             Console.WriteLine("--------------------");
 
             //assume n is some string
-            cmd = new SQLiteCommand("select users.name as BLAH, count(posts.id) as BOOM from users " +
+            cmd = new SQLiteCommand("select users.name as BLAH, count(posts.pid) as BOOM from users " +
             "left outer join posts on users.uid=posts.uid group by users.name " +
-            "order by count(posts.id) desc;", conn);
+            "order by count(posts.pid) desc;", conn);
             rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
